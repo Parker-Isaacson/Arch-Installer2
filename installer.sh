dc01c7f6b8993c3328c63ec9ca9be6ca48a6c739
Parker-Isaacson <parker.isaacson@gmail.com>
2025-04-27 11:38:21 -0400

    Finalized

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 
 user=""
 pass=""
-swap="False"      # Build without swap partition
+swap="False"         # Build without swap partition
 no_wm="False"        # Build with window manager 
 hostname="default"   # Default hostname
 region="America"     # Default time zone being America/New_York
@@ -96,6 +96,82 @@ echo "region=$region"
 echo "city=$city"
 echo "drive=$drive"
 
+# Start the install process:
+stty -echo
+
+# Starting things for the time and drives
+timedatectl
+wipefs --all "$drive"
+
+# Partition the drive with swap
 if [ "$swap" = "True" ]; then
-    echo "Swapping"
+    echo -e "g\nn\n\n\n+1G\ny\nn\n\n\n+4G\ny\nn\n\n\n\ny\nw\nq" | fdisk "$drive"
+    mkfs.fat -F 32 "$drive"1
+    mkswap "$drive"2
+    mkfs.ext4 -F "$drive"3
+    mount "$drive"3 /mnt
+    swapon "$drive"2
 fi
+
+# Partition the drive without swap
+if [ "$swap" = "False" ]; then
+    echo -e "g\nn\n\n\n+1G\ny\nn\n\n\n\ny\nw\nq" | fdisk "$drive"
+    mkfs.fat -F 32 "$drive"1
+    mkfs.ext4 -F "$drive"2
+    mount "$drive"2 /mnt
+fi
+
+# Create the package list
+packages=""
+
+if [ "$no_wm" = "False" ]; then # If we are using a wm
+    packages="gcc gdb pipewire pipewire-pulse xorg xorg-xinit i3-gaps alacritty i3status dmenu vim"
+fi
+
+if [ "$no_wm" = "True" ]; then # If we are not using a wm
+    packages="gcc gdb vim"
+fi
+
+# Configure the system
+pacstrap /mnt base linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt /bin/bash << EOF
+ln -sf /usr/share/zoneinfo/$region/$city /etc/localtime
+hwclock --systohc
+
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+echo "$hostname" > /etc/hostname
+echo -e "127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\t$hostname.localdomain\t$hostname" > /etc/hosts
+
+echo -e "$pass\n$pass" | passwd
+
+pacman -Sy
+pacman -S --noconfirm grub efibootmgr sudo networkmanager
+
+systemctl enable NetworkManager
+systemctl start NetworkManager
+
+mkdir /boot/EFI
+mount ${drive}1 /boot/EFI
+
+grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+useradd -m -G wheel $user
+echo -e "$pass\n$pass" | passwd $user
+echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
+
+pacman -S --noconfirm $packages
+echo "exec i3" > /home/$user/.xinitrc
+
+exit
+EOF
+
+umount -R /mnt
+
+stty echo
+
+echo "System Is Setup, please reboot"
